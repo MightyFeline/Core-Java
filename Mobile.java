35efc7d6c340ab6f0147058ef70bff2f8e49b620
madhumitha-6 <madhumitha.xworkz@gmail.com>
2024-06-24 10:37:13 +0530

    First Program

---
+++ Mobile.java
@@ -0,0 +1,16 @@
+class Mobile{
+
+	public static void main(String [] args){
+		String brandName = "Samsung";
+		float price = 12,985F;
+		String os = "Android 13.0";
+		int ramSize = 6;
+		int cpuSpeed = 2.4;
+		int memory = 128;
+		System.out.println("Brand Name of Mobile:" + brandName);
+		System.out.println("Price pf Mobile:" + price);
+		System.out.println("OS of Mobile:" + os);
+		System.out.println("CPU Speed of Mobile:" + cpuSpeed);
+		System.out.println("Memory Storage Capacity of Mobile:" + memory);
+	}
+}
